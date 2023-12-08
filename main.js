33ddc84949ebfbef675971d24f21f3dfb757b32f
LeCehlou <iscoandyoung@gmail.com>
2023-12-08 05:09:34 +0100

    c2

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 //Variables affichées
-let climatValue = 0.5
-let pollutionValue = 0.7
-let ressourceValue = 0.3
+let climatValue
+let pollutionValue 
+let ressourceValue
+let energieValue
 
 //Variables sub
-let co2 = 0;
-let dechet = 0;
-
-let temperature = 0;
-let catastrophe = 0;
-let ozone = 0;
-
-let nature = 0;
-let metal = 0;
-let fossile = 0;
-
+var co2 = 10
+var dechet= 10
+var ozone = 0
+var temperature= 10
+var nature= 15
+var fossile = 0
+var metal= 0
+var nucleaire = 0
+var production = 5
+var consommation = 2
 //Accomplissements 
 let hasPetrole = false;
 let hasInternet = false;
@@ -23,7 +23,10 @@ let hasNuclear = false;
 //Variables fonctionnement jeu
 let listeAnnees = [{annee : 1770 , fixe : true}, {annee : 1950, fixe : true}, {annee : 1987, fixe : true}, {annee : 1990, fixe : true}, {annee : 2000, fixe : false}, {annee : 2005, fixe : false}, {annee : 2010, fixe : false}, {annee : 2015, fixe : false}, {annee : 2020, fixe : true}, {annee : 2023, fixe : false}]
 let nombreAnnes = listeAnnees.length
-let indexAnnee = 0;
+let indexAnnee = 0
+enAttente = false
+question = null
+choix = null
 
 function showModal() {
     var modal= document.getElementById("InformationModale")
@@ -31,19 +34,22 @@ function showModal() {
     }
 function closeModal() {
     var modal= document.getElementById("InformationModale")
-    modal.style.display = "none";
+            modal.style.display = "none";
 }
 
 function calculateBars() {
     pollutionValue = (co2 + dechet) / 2
-    climatValue = (temperature + catastrophe + ozone) / 3
+    climatValue = (temperature + ozone) / 2
     ressourceValue = (nature + metal + fossile) / 3
+    energieValue = (production + consommation) / 2
+    console.log(pollutionValue)
 }
 
 function updateProgressBars() {
     document.getElementById('climat').value = climatValue;
     document.getElementById('pollution').value = pollutionValue;
     document.getElementById('ressource').value = ressourceValue;
+    document.getElementById('energie').value = energieValue
 }
 
 function showCards() {
@@ -52,21 +58,28 @@ function showCards() {
 
 function playTurn(question) {
     showCards(question)
-    while (true) {
-        //Besoin d'attendre un listener 
+    enAttente = true
+    while (enAttente);
 
-    }
-    //Récupérer le bon choix
-    //choix = [....]
-    //eval(choix.consequences)
     if (question.modale != null) {
-        //On charge liée
+        //showModal(que)
     }
 }
 
+function onClickCard(number) {
+    enAttente = false
+    if (number == 1) {
+        choix = question.question.choix1 
+    }
+    else {
+        choix = question.question.choix2
+    }
+    eval(choix.consequence)
+    
+}   
 function gameLoop() {
     while (true) {
-        const question = getQuestionActuelle(listeAnnees[indexAnnee].fixe)
+        question = getQuestionActuelle(listeAnnees[indexAnnee].fixe)
         playTurn(question)
         calculateBars()
         updateProgressBars() 
@@ -76,3 +89,37 @@ function gameLoop() {
         }
     }
 }
+
+
+async function playTurnAsync(question) {
+    return new Promise((resolve) => {
+        showCards(question);
+        enAttente = true;
+
+        function checkCondition() {
+            if (!enAttente) {
+                resolve();
+            } else {
+                setTimeout(checkCondition, 0);
+            }
+        }
+
+        checkCondition();
+    });
+}
+
+async function gameLoopAsync() {
+    for (let i = 0; i < nombreAnnes; i++) {
+        question = getQuestionActuelle(listeAnnees[indexAnnee].fixe);
+        await playTurnAsync(question);
+        indexAnnee++;
+        calculateBars();
+        updateProgressBars();
+        if (indexAnnee > nombreAnnes - 1) {
+            break;
+        }
+    }
+}
+
+// Call gameLoopAsync somewhere in your code
+gameLoopAsync();
--- questions.js
+++ questions.js
@@ -1,17 +1,111 @@
-const json = {
-    questions : [
-        {
-            condition : "false",
-            fixe : false,
-            poids : 50
-        },
-        {
-            condition : "!hasPetrole",
-            fixe : false,
-            poids : 30
-        }
-    ]
-}
+
+const json = {questions : [
+    {
+        texte : "création de la machine à vapeur :",
+        condition : "false",
+        choix1:
+            {
+                texte: "Utilisation généralisé",
+                consequence: "",
+                img:""
+            }
+        ,
+        choix2:
+            {
+                texte: "Utilisation modéré",
+                consequence: "",
+                img:""
+            }
+        ,
+        fixe : true,
+        poids : 50
+    },
+
+    {
+        texte : "Première central nucléaire :",
+        condition : "false",
+        choix1:
+            {
+                texte: "Investir dans le nucléaire",
+                consequence: "",
+                img:""
+            }
+        ,
+        choix2:
+            {
+                texte: "Rester au charbon",
+                consequence: "",
+                img:""
+            }
+        ,
+        fixe : true,
+        poids : 50
+    },
+
+    {
+        texte : "Première éolienne :",
+        condition : "false",
+        choix1:
+            {
+                texte: "Investir dans l'éolien",
+                consequence: "",
+                img:""
+            }
+        ,
+        choix2:
+            {
+                texte: "Continuer à investir dans le nucléaire",
+                consequence: "",
+                img:""
+            }
+        ,
+        fixe : true,
+        poids : 50
+    },
+
+    {
+        texte : "Premier rapport du giec :",
+        condition : "false",
+        choix1:
+            {
+                texte: "Commencer à prévenir et à agir",
+                consequence: "",
+                img:""
+            }
+        ,
+        choix2:
+            {
+                texte: "Investir dans les technologies",
+                consequence: "",
+                img:""
+            }
+        ,
+        fixe : false,
+        poids : 50
+    },
+
+    {
+        texte : "Les trajets polue il faudrait arranger ça",
+        condition : "true",
+        choix1:
+            {
+                texte: "Ivestir dans les voiture électrique",
+                consequence: "co2 += 100;",
+                img:""
+            }
+        ,
+        choix2:
+            {
+                texte: "Investir dans les transports en communs",
+                consequence: "",
+                img:""
+            }
+        ,
+        fixe : true,
+        poids : 50
+    },
+
+]}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -36,7 +130,6 @@ function getQuestionActuelle(roundFixe) {
     });
     const maxRange = listQuestionsValables[listQuestionsValables.length - 1].max;
     const rolledNumber = getRandomInt(0,maxRange)
-    console.log(listQuestionsValables)
 
     return selectedQuestion = listQuestionsValables.find(
         question => rolledNumber >= question.min && rolledNumber <= question.max
